2287f44d72d32ccae1cfea99a191126217896b73
Nathan Rosario <nathanrosario@ac.c4q.nyc>
2018-01-18 16:36:22 -0500

    feat(Cocoapods)

---
+++ Pods/Target Support Files/Pods-Unit5GroupProjectOneUITests/Pods-Unit5GroupProjectOneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Unit5GroupProjectOneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Unit5GroupProjectOneUITests
+@end
--- Unit5GroupProjectOne/ViewControllers/SearchViewController.swift
+++ Unit5GroupProjectOne/ViewControllers/SearchViewController.swift
@@ -7,18 +7,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchViewController: UIViewController {
 
+    var someView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        someView.backgroundColor = .red
+        view.addSubview(someView)
+        someView.snp.makeConstraints { (view) in
+            let safe = self.view.safeAreaLayoutGuide
+            view.top.bottom.leading.trailing.equalTo(safe)
+//            view.width.height.equalTo(200)
+//            view.center.equalToSuperview()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }

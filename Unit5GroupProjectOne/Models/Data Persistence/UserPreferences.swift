b77c462eb126a5d41367f4cc99bb377b1e71b0e1
Melissa He <melissahe@ac.c4q.nyc>
2018-01-22 17:49:01 -0500

    Add image cache model and user preferences to store search term and location

---
+++ Unit5GroupProjectOne/Models/Data Persistence/UserPreferences.swift
@@ -0,0 +1,17 @@
+//
+//  UserPreferences.swift
+//  Unit5GroupProjectOne
+//
+//  Created by C4Q on 1/22/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import Foundation
+
+class UserPreferences {
+    
+    //should save location
+    
+    //should save user search term in search bar
+    
+}

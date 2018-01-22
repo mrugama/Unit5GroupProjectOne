d637a68147d7391722bc2c362e8e7da845525121
Melissa He <melissahe@ac.c4q.nyc>
2018-01-21 19:42:29 -0500

    Changed the constraints of the collection view so the cells are bigger, also added custom collection view cell and view controller for table view results

--- Unit5GroupProjectOne/View Controllers/SearchViewController.swift
+++ Unit5GroupProjectOne/View Controllers/SearchViewController.swift
@@ -12,7 +12,9 @@ import MapKit
 
 class SearchViewController: UIViewController {
     
-    let searchView = SearchView()
+    private let cellSpacing: CGFloat = UIScreen.main.bounds.width * 0.02
+    
+    private let searchView = SearchView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +31,8 @@ class SearchViewController: UIViewController {
         navigationItem.searchController?.searchBar.placeholder = "Search for a venue"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "tableview icon"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(venueListButton))
     }
-    @objc func venueListButton() {
-        let modalVC = UIViewController()
+    @objc private func venueListButton() {
+        let modalVC = VenueListViewController() //to do: the initializer for this view controller should take in the current results (the data source variable) and pass them to this view controller, so it has a datasource variable for its own table view
         let navController = UINavigationController(rootViewController: modalVC)
         modalVC.view.backgroundColor = .white
         self.present(navController, animated: true, completion: nil)
@@ -49,13 +51,39 @@ class SearchViewController: UIViewController {
         }
     }
 }
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCells: CGFloat = 4.5
+        let numberOfSpaces: CGFloat = numberOfCells + 1
+        let width = (collectionView.bounds.width - (numberOfSpaces * cellSpacing)) / numberOfCells
+        let height = width
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: cellSpacing, bottom: 0, right: cellSpacing)
+    }
+    
+}
+
 extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 10 //to do: change when we have a working datasource variable!!
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionCell", for: indexPath) as! SearchCollectionViewCell
         cell.contentView.backgroundColor = .gray
         return cell
     }

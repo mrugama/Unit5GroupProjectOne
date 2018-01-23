d90c5cebbc1b0655611a5b4f5dff53d10b2c9c97
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2018-01-23 15:10:11 -0500

    Refactored VenueAPIClient and added APIKeys file

--- Unit5GroupProjectOne/Models/VenueAPIClient.swift
+++ Unit5GroupProjectOne/Models/VenueAPIClient.swift
@@ -11,45 +11,35 @@ import Alamofire
 class VenueAPIClient {
     private init() {}
     static let manager = VenueAPIClient()
-    private let keyAPI = "ZQSWSRCGU33XMP2KNS2BWUNUICV4JJQYQ2NJNUNRTT4SXEZT"
-    func getVenues(withSearchTerm searchTerm: String,
-                   lat latitude: Double,
+    func getVenues(lat latitute: Double,
                    lon longitude: Double,
-                   completion: @escaping ([Venue]) -> Void,
-                   errorHandler: @escaping (Error) -> Void) {
-        let urlWeatherBase = "https://api.foursquare.com/v2/venues/search?"
+                   completion: @escaping ([Venue]?) -> Void) {
+        let urlBase = "https://api.foursquare.com/v2/venues/search"
+        let dateFormatted = DateFormatter()
+        let date = Date()
+        dateFormatted.dateFormat = "yyyyMMdd"
+        let strDate = dateFormatted.string(from: date)
+        print(strDate)
+        let params: [String: Any] = ["ll": "\(latitute),\(longitude)",
+            "oauth_token": APIKeys.apiKey,
+            "v": strDate]
         
-        let params: [String: Any] = [
-            "query": searchTerm,
-            "ll": "\(latitude),\(longitude)",
-            "oauth_token": keyAPI,
-            "v": "20180118"
-        ]
-        
-        Alamofire.request(urlWeatherBase, method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON{ response in
-            
-            switch response.result{
-            case .success:
-                if let data = response.data {
-                    do {
-                        let result = try JSONDecoder().decode(AllVenue.self, from: data)
-                        completion(result.responseVenue.venues)
-                    } catch let error {
-                        print("Error decoding: \(error.localizedDescription)")
-                        errorHandler(AppError.couldNotParseJSON(rawError: error))
-                    }
-                }
-                
-            // MARK: do whatever you want
+        Alamofire.request(urlBase, method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            switch dataResponse.result {
             case .failure(let error):
-                
-                if let error = error as? URLError {
-                    if error.code == URLError.notConnectedToInternet {
-                        errorHandler(AppError.noInternetConnection)
+                print("Response error: \(error.localizedDescription)")
+            case .success:
+                if let dataError = dataResponse.error {
+                    print("Data Error: \(dataError.localizedDescription)")
+                } else if let data = dataResponse.data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let results = try decoder.decode(AllVenue.self, from: data)
+                        completion(results.responseVenue.venues)
+                    } catch let error {
+                        print("Data struct error: \(error.localizedDescription)")
                     }
                 }
-                
-                errorHandler(AppError.other(rawError: error))
             }
         }
     }

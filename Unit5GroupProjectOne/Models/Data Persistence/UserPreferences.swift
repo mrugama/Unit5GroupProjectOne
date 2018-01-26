262ecea3e38b45075da28878cf66ce29ef44ca2d
Melissa He <melissahe@ac.c4q.nyc>
2018-01-26 12:50:58 -0500

    Add user preferences class to store user location and search location

--- Unit5GroupProjectOne/Models/Data Persistence/UserPreferences.swift
+++ Unit5GroupProjectOne/Models/Data Persistence/UserPreferences.swift
@@ -9,9 +9,30 @@
 import Foundation
 
 class UserPreferences {
+    private init() {}
     
-    //should save location
+    static let manager = UserPreferences()
     
-    //should save user search term in search bar
+    private let userDefaults = UserDefaults.standard
+    private let userCoordinatesKey = "User Coordinates Key"
+    private let searchCoordinatesKey = "Search Coordinates Key"
     
+    //save
+    public func saveUserCoordinates(latitude: Double, longitude: Double) {
+        let userCoordinates: (latitude: Double, longitude: Double) = (latitude, longitude)
+        
+        userDefaults.set(userCoordinates, forKey: userCoordinatesKey)
+    }
+    public func saveSearchCoordinates(latitude: Double, longitude: Double) {
+        let searchCoordinates: (latitude: Double, longitude: Double) = (latitude, longitude)
+        
+        userDefaults.set(searchCoordinates, forKey: searchCoordinatesKey)
+    }
+    //get
+    public func getUserCoordinate() -> (latitude: Double, longitude: Double)? {
+        return userDefaults.object(forKey: userCoordinatesKey) as? (Double, Double)
+    }
+    public func getSearchCoordinates() -> (latitude: Double, longitude: Double)? {
+        return userDefaults.object(forKey: searchCoordinatesKey) as? (Double, Double)
+    }
 }
